0a56a5ee666a37b42d710456e6696fb9b695c59b
anil <ghimireanil96@gmail.com>
2021-02-27 03:04:46 -0500

    first trial

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-const PORT = 5000 || env.PORT;
+dotenv.config();
+//import routes
+const authRoute = require('./routes/auth');
+
+// connect to db
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  },
+  () => console.log('Connected to DB')
+);
+
+//Middlewares
+app.use(express.json());
+
+//Route middlewares
+app.use('/auth', authRoute);
+
+const PORT = 3000 || env.PORT;
 app.listen(PORT, () => console.log(`Server is listning on port ${PORT}...`));
